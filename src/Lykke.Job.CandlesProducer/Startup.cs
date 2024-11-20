965eee04ec94eacb29cf1579a1af361ea13caf60
Dmitry Samodov <samodov.dmitry@gmail.com>
2024-11-20 10:06:58 +0300

    feat(LT-5879) - updated LykkeBiz.SettingsReader to 9.2.0

--- src/Lykke.Job.CandlesProducer/Startup.cs
+++ src/Lykke.Job.CandlesProducer/Startup.cs
@@ -168,7 +168,7 @@ namespace Lykke.Job.CandlesProducer
             app.UseRouting();
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
-                endpoints.AddSettingsTemplateEndpoint();
+                endpoints.MapSettingsTemplate();
             });
             app.UseSwagger(c =>
             {
